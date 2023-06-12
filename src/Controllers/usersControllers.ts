df96cd5aea36ca03371eef4bf880946c543883be
Nehuen Cisneros <cisneros.n.b@outlook.com>
2023-06-12 13:01:08 -0300

    borrado de comentarios en usersControllers

--- src/Controllers/usersControllers.ts
+++ src/Controllers/usersControllers.ts
@@ -7,36 +7,3 @@ export const createUser = async (user:any) => {
     return newUser
 
 }
-
-// const createNewActivity = async (name, difficulty, duration, season, country) => {
-//     await getAllCountriesInfo()
-
-//     const countrySelected = await Country.findAll({
-//         where: { name : country }
-//     })
-
-//     const repeatActivity = await Activity.findOne({ 
-//         where: { 
-//             name: name, 
-//             difficulty: difficulty, 
-//             duration: duration,
-//             season: season 
-//         } 
-//     });
-
-//     if(!repeatActivity){
-//         const newActivity = await Activity.create({
-//             name, 
-//             difficulty, 
-//             duration, 
-//             season,
-//         });
-//         const activityCountry = await newActivity.addCountry(countrySelected)
-
-//         return activityCountry 
-//     } else {
-//         const activityCountry = await repeatActivity.addCountry(countrySelected)
-
-//         return activityCountry;
-//     }
-// }
