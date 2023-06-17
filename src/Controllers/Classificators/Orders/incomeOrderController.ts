6cde3ff266236c40d20a0723835c3c66cbdf8aca
Nehuen Cisneros <cisneros.n.b@outlook.com>
2023-06-17 19:13:53 -0300

    autenticacion terminada

---
+++ src/Controllers/Classificators/Orders/incomeOrderController.ts
@@ -0,0 +1,15 @@
+
+// export const expenseAlphaOrder= async(alpha: IAlpha, id: number)=>{
+    
+//     const arrayAccount = await getAllIdExpense(id);
+//     const arrayExpenses = arrayAccount?.map((e)=> e?.expense)
+//     let resultsArray:any = [];
+
+//     const resultsFinal = arrayExpenses.map((element)=> element.filter(e => resultsArray.push(e) ))
+
+//     const results = (alpha.alpha === "A") ? resultsArray.sort((a: any, b: any) => a.description.localeCompare(b.description))
+//     : resultsArray.sort((a:any, b: any) => b.description.localeCompare(a.description));
+
+//     return results
+
+// }
---
+++ src/Handlers/Classificators/Orders/incomeOrderHandler.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+
+export interface IAlpha {
+    alpha: string;
+}
+
+export const getIncomeAlphaOrder = async (req: Request, res: Response) => {
+
+};
+
+export const getIncomeAmountOrder = async (req: Request, res: Response) => {
+
+};
+
+export const getIncomeDateOrder = async (req: Request, res: Response) => {
+
+    
+};
