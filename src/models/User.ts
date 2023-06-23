9b6c4554b801ff394ab0d3faf465cec797f70d39
guillerminaDiaz <guillerminaadiaz@gmail.com>
2023-06-23 17:00:53 -0300

    user changes

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,61 @@
+// import {
+//   Model,
+//   Column,
+//   Table,
+//   CreatedAt,
+//   UpdatedAt,
+//   HasOne
+// } from "sequelize-typescript";
+// import { Balance } from "./Balance";
+
+// @Table
+// export class User extends Model<User> {
+//   @Column({
+//     primaryKey: true,
+//     autoIncrement: true,
+//   })
+//   id!: number;
+
+//   @Column({
+//     allowNull: false,
+//   })
+//   name!: string;
+
+//   @Column({
+//     allowNull: false,
+//     unique: true
+//   })
+//   email!: string;
+
+//   @Column({
+//     allowNull: false,
+//     unique: true
+//   })
+//   password!: string;
+
+//   // @Column({
+//   //   allowNull: true,
+//   // })
+//   // picture!: string;
+
+//   @Column({
+//     allowNull: false,
+//     defaultValue: false,
+//   })
+//   premium!: boolean;
+
+//   @CreatedAt
+//   @Column
+//   createdAt!: Date;
+
+//   @UpdatedAt
+//   @Column
+//   updatedAt!: Date;
+
+//   @HasOne(() => Balance)
+//   balance!: Balance;
+// }
+
 import {
   Model,
   Column,
@@ -33,10 +91,11 @@ export class User extends Model<User> {
   })
   password!: string;
 
-  // @Column({
-  //   allowNull: true,
-  // })
-  // picture!: string;
+  @Column({
+    defaultValue:"https://i.scdn.co/image/ab67706c0000da84508c76d25acfbf1dc9fc0282" ,
+    allowNull: true
+  })
+  picture!: string;
 
   @Column({
     allowNull: false,
